e6b3afaf333c14dc355ec8b5c39c192430da2b38
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-11-03 13:02:04 +0300

    start container for header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './style.scss'
+
+const Header = () => {
+	return (
+		<>
+			<div className={styles.container}>
+				<h1>header</h1>
+			</div>
+		</>
+	);
+};
+
+export default Header;
