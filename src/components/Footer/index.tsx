58a2e0aa59590695ce1f29d33747a2c04d75bc03
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-11-09 15:41:15 +0300

    footer basis and types for search

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Container from '../Container'
+import { Link } from 'react-router-dom'
+
+import styles from './style.module.scss'
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear()
     return (
         <>
-            <h1>footer</h1>
+            <footer className={styles.footer}>
+                <Container>
+                    <div className={styles.wrapper}>
+                        <span>Reading books APP</span>
+                        <nav className={styles.footerNavigation}>
+                            <Link to='/'>About</Link>
+                            <Link to='/'>Contacts</Link>
+                        </nav>
+                        <span>Copyright © {currentYear} <a href="mailto:Lukdmitrij@yandex.ru">Lukdmitrij@yandex.ru</a>  All Rights Reserved</span>
+                    </div>
+                </Container>
+            </footer>
         </>
     );
 };
